8231dbe0e98cc07e2e04152df175e0190699b4e5
Nora Moser <nora.moser@palinode.io>
2025-03-07 15:33:36 -0800

    practicing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { TaskProvider } from "./TaskContext"; // Import TaskProvider
-import TaskManager from "./TaskManager"; // We'll move UI logic to a separate component
+import { TaskProvider } from "./TaskStuff/TaskContext"; // Import TaskProvider
+import TaskManager from "./TaskStuff/TaskManager"; // We'll move UI logic to a separate component
+import styles from './TaskStuff/Task.module.css';
 
 function App() {
   return (
     <TaskProvider>
       <div className="App">
-        <h1>Task Manager</h1>
+        <h1 className={styles.title}>Task Manager</h1>
         <TaskManager />
       </div>
     </TaskProvider>
